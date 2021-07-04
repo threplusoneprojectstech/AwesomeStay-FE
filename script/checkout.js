271373482cdbc89d5e8c2292b67124b0c7f55371
cornelius tantius <corneliustantius.ct@gmail.com>
2021-07-04 10:19:58 +0700

    add checkout api hit

--- script/checkout.js
+++ script/checkout.js
@@ -1,21 +1,86 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+function ItemBuilder(itemName, price, itemId){
+  return `
+    <li class="list-group-item d-flex justify-content-between lh-sm">
+        <div>
+            <h6 class="my-0">${itemName}</h6>
+            <small class="text-muted">${price}</small>
+        </div>
+        <span class="text-muted"><a href="./details.html?iid=${itemId}">Details</a></span>
+    </li>
+  `
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const trxId = urlParams.get('trx');
+var TotalPrice = 0;
+var settings = {
+  "url": "https://awesome-stay-api.herokuapp.com/transaction/getspecific",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+      "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+      "id":trxId
+  }),
+};
+$.ajax(settings).done(function (response) {
+  if(response.status == 1){
+      window.localStorage.clear();
+      res = response["result"];
+      document.getElementById("itcountspan-blue").innerHTML = res.orders.length;
+      res.orders.forEach(element => {
+        var set = {
+          "url": "https://awesome-stay-api.herokuapp.com/product/getdetails",
+          "method": "POST",
+          "timeout": 0,
+          "headers": {
+              "Content-Type": "application/json"
+          },
+          "data": JSON.stringify({
+              "id":element
+          }),
+        };
+        $.ajax(set).done(function(response){
+          if(response.status == 1){
+            result = response["result"];
+            let itemName = result["productName"];
+            let price = result["price"];
+            let item_id = result["_id"];
+            let doc = document.getElementById("itemlist-view");
+            TotalPrice += price;
+            document.getElementById("strongprice").innerHTML = "Rp. " + parseInt(TotalPrice);
+            doc.innerHTML = doc.innerHTML+=ItemBuilder(itemName, price, item_id);
           }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
-  
+          else{
+            console.log(response);
+          }
+        })
+      });
+  }
+  else{
+      alert(response.message);
+      return;
+  }
+});
